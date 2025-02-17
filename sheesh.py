3029eab7126a13a5d706c0ebe4fb0335e68b6db2
risu897 <criscorpuz301@gmail.com>
2025-02-17 14:55:56 +0800

    Add files via upload

---
+++ sheesh.py
@@ -0,0 +1,11 @@
+#Python Program Dictionary
+data = {
+"Python" : "a programming language that is widely used in web applications, software development, data science, and machine learning",
+"Programming" : "Computer programming or coding is the composition of sequences of instructions, called programs, that computers can follow to perform tasks.",
+"Syntax" : "Syntax is the set of rules that define what the various combinations of symbols mean",
+"Code" : "Code refers to the set of instructions, or a system of rules, written in a particular programming language (i.e., the source code)."
+"Script" : "A script is a program or sequence of instructions that is interpreted or carried out by another program rather than by the computer processor (as a compiled program is)."
+ }
+
+a = input("Say:")
+print(data[a])
